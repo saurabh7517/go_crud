8367aac92e92a39ef3e3ccfc4640f98159866ab5
Saurabh Kumar <saurabh7517@gmail.com>
2024-02-07 08:07:14 +0000

    renaming controller

--- src/main.go
+++ src/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	restwrapper "crud/src/pkg/controller"
+	controller "crud/src/pkg/controller"
 	"net/http"
 )
 
 func main() {
 	var mux *http.ServeMux = http.NewServeMux()
-	restwrapper.RegisterRestController(mux)
+	controller.RegisterRestController(mux)
 	http.ListenAndServe(":8080", mux)
 
 }
