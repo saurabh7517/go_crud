5e29889b78210d494be8a1507f5665a2f13366c6
Saurabh Kumar <saurabh7517@gmail.com>
2024-02-07 08:09:37 +0000

    writing service layer

---
+++ src/pkg/service/service.go
@@ -0,0 +1,77 @@
+package service
+
+import "errors"
+
+type Movie struct {
+	Id       int       `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	firstname string `json: "firstname"`
+	lastname  string `json: "lastname"`
+}
+
+var movies []Movie
+
+var unknownMovie Movie = Movie{-1, "garbage", "garbage", &Director{"garbage", "garbage"}}
+
+func GenerateMovieList() {
+	movies = append(movies, Movie{1, "123341", "The New Gig", &Director{"John", "Doe"}})
+	movies = append(movies, Movie{2, "123342", "Wrecmeister Harmonies", &Director{"Bela", "Tar"}})
+	movies = append(movies, Movie{3, "123343", "In Vanda's Room", &Director{"Pedro", "Costa"}})
+	movies = append(movies, Movie{4, "123344", "The Intruder", &Director{"Claire", "Denis"}})
+	movies = append(movies, Movie{5, "123345", "Morvern Callar", &Director{"Lynne", "Ramsay"}})
+	movies = append(movies, Movie{6, "123346", "Possession", &Director{"Andrej", "Zulawski"}})
+	movies = append(movies, Movie{7, "123347", "Earth", &Director{"Alaxender", "Dovzhenko"}})
+	movies = append(movies, Movie{8, "123348", "Annie Hall", &Director{"Woody", "Allen"}})
+	movies = append(movies, Movie{9, "123349", "Sullivan Travels", &Director{"Preston", "Sturges"}})
+	movies = append(movies, Movie{10, "123350", "Pandora's Box", &Director{"G.W", "Pabst"}})
+	movies = append(movies, Movie{11, "123351", "Born in Flames", &Director{"Lizzie", "Borden"}})
+	movies = append(movies, Movie{12, "123352", "Grave of the Fireflies", &Director{"Isao", "Takahata"}})
+}
+
+func GetAllMovies() []Movie {
+	if len(movies) > 0 {
+		return movies
+	} else {
+		GenerateMovieList()
+		return movies
+	}
+}
+
+func GetMovieById(id int) (Movie, error) {
+	for _, v := range movies {
+		if id == v.Id {
+			return v, nil
+		}
+	}
+	return unknownMovie, errors.New("Not Found")
+}
+
+func AddNewMovie(newMovie Movie) {
+	movies = append(movies, newMovie)
+}
+
+func UpdateMovie(newMovie Movie) {
+	var found bool = false
+	for _, movie := range movies {
+		if movie.Id == newMovie.Id {
+			updateMovie(movie, newMovie)
+			found = true
+			break
+		}
+	}
+	if !found {
+		AddNewMovie(newMovie)
+	}
+}
+
+func updateMovie(oldMovie Movie, newMovie Movie) {
+	oldMovie.Id = newMovie.Id
+	oldMovie.Isbn = newMovie.Isbn
+	oldMovie.Title = newMovie.Title
+	oldMovie.Director = newMovie.Director
+}
