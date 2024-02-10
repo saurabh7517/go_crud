6def46a3be13f1fd1d38f1127e74b163daa18249
Saurabh Kumar <saurabh7517@gmail.com>
2024-02-10 07:51:46 +0000

    loading movie list on start up

--- src/main.go
+++ src/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	controller "crud/src/pkg/controller"
+	"crud/src/pkg/service"
 	"net/http"
 )
 
 func main() {
 	var mux *http.ServeMux = http.NewServeMux()
 	controller.RegisterRestController(mux)
+	service.GenerateMovieList()
 	http.ListenAndServe(":8080", mux)
-
 }
